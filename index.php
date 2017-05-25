2cdf956d6778b722bd9f9369d6bc1350fceef897
voarsh <voarsh@users.noreply.github.com>
2017-05-25 01:45:03 +0100

    Create test index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<head>
+<title> Test page </title>
+</head>
+
+<body>
+<h1> You've reached a test page! </h1> <br />
+
+<p> If you've gotten here by accident, please accept this as a 404! </p>
+</body>
